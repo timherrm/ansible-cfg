1f96e35d382dbd2b1aa3e99f140e6440dfcf0d47
timo <timherrm@users.noreply.github.com>
2023-07-25 12:00:48 +0200

    Create setup_ansible.sh

---
+++ setup_ansible.sh
@@ -0,0 +1,8 @@
+apt install pipx git -y &&
+pipx install --include-deps ansible &&
+pipx ensurepath &&
+pipx inject ansible argcomplete &&
+echo 'eval "$(register-python-argcomplete pipx)"' >> /root/.bashrc &&
+activate-global-python-argcomplete --user &&
+git clone https://github.com/timherrm/ansible-cfg &&
+reboot
